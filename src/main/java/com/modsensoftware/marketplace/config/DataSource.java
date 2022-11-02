44aebdaf3e51361cd61a730fae44745f39c8f677
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-02 16:07:05 +0300

    JDBC Datasource setup with HikariCP

---
+++ src/main/java/com/modsensoftware/marketplace/config/DataSource.java
@@ -0,0 +1,27 @@
+package com.modsensoftware.marketplace.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author andrey.demyanchik on 11/1/2022
+ */
+public class DataSource {
+
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource dataSource;
+
+    static {
+        config.setJdbcUrl("jdbc:postgresql://localhost:32768/marketplace");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        dataSource = new HikariDataSource();
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
