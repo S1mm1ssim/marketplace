d7290273f419a909fc1a015df385ae991e1763c8
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-17 23:32:37 +0300

    Add utility methods

--- src/main/java/com/modsensoftware/marketplace/utils/Utils.java
+++ src/main/java/com/modsensoftware/marketplace/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.modsensoftware.marketplace.utils;
 
 import java.util.Objects;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 /**
@@ -12,5 +13,21 @@ public class Utils {
         if (Objects.nonNull(value)) {
             setter.accept(value);
         }
+    }
+
+    public static <R, U> void setIfNotNull(R value1, U value2, BiConsumer<R, U> setter) {
+        if (Objects.nonNull(value1) && Objects.nonNull(value2)) {
+            setter.accept(value1, value2);
+        }
+    }
+
+    public static <R, U> void putIfNotNull(R key, U value, BiConsumer<R, U> put) {
+        if (Objects.nonNull(key) && Objects.nonNull(value)) {
+            put.accept(key, value);
+        }
+    }
+
+    public static String wrapIn(String value, String symbol) {
+        return symbol + value + symbol;
     }
 }
