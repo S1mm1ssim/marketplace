1f8ebf9de34a1d0dc8ea0fb4ca39b3c39158612e
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-22 20:28:28 +0300

    Implement Testcontainers setup:
    - single container init for all tests
    - on init dynamically set connection properties for Hibernate SessionFactory

---
+++ src/test/java/com/modsensoftware/marketplace/integration/AbstractIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.modsensoftware.marketplace.integration;
+
+import com.modsensoftware.marketplace.CustomPostgreSQLContainer;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.jdbc.JdbcDatabaseDelegate;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * @author andrey.demyanchik on 11/22/2022
+ */
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = {
+                "server.port=8081",
+                "management.server.port=9081"
+        })
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    public static CustomPostgreSQLContainer postgreSQLContainer
+            = CustomPostgreSQLContainer.getInstance();
+
+    protected static final JdbcDatabaseDelegate dbDelegate
+            = new JdbcDatabaseDelegate(postgreSQLContainer, "");
+
+    @BeforeAll
+    static void beforeAll() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = 8081;
+        } else {
+            RestAssured.port = Integer.parseInt(port);
+        }
+
+        String basePath = System.getProperty("server.base");
+        if (basePath == null) {
+            basePath = "/api/v1";
+        }
+        RestAssured.basePath = basePath;
+    }
+}
