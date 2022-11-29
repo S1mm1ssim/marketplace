76df2e11bff7193b5679b446e4918f0f33284283
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-29 15:35:12 +0300

    Make Constants.java final

--- src/main/java/com/modsensoftware/marketplace/constants/Constants.java
+++ src/main/java/com/modsensoftware/marketplace/constants/Constants.java
@@ -3,7 +3,7 @@ package com.modsensoftware.marketplace.constants;
 /**
  * @author andrey.demyanchik on 11/29/2022
  */
-public class Constants {
+public final class Constants {
 
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String EMAIL_FILTER_NAME = "email";
