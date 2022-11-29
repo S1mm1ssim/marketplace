18e94383c56d8c2eba679b8c565f7a97f043d4bb
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-29 18:47:32 +0300

    Refactor CompanyDao unit tests
    - Remove code duplication
    - Replace setter object creation with constructor

--- src/test/java/com/modsensoftware/marketplace/unit/company/CompanyDaoTest.java
+++ src/test/java/com/modsensoftware/marketplace/unit/company/CompanyDaoTest.java
@@ -15,9 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -25,6 +24,8 @@ import java.util.Map;
 import java.util.Random;
 
 import static java.lang.String.format;
+import static java.time.LocalDateTime.now;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * @author andrey.demyanchik on 11/23/2022
@@ -45,30 +46,32 @@ public class CompanyDaoTest {
 
     @Value("${default.page.size}")
     private int pageSize;
+    @Value("${exception.message.companyNotFound}")
+    private String companyNotFoundMessage;
 
     @Test
     public void canSaveCompany() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
+
         // when
         underTest.save(company);
+
         // then
         Company saved = sessionFactory.openSession().get(Company.class, company.getId());
         Assertions.assertThat(saved).isEqualTo(company);
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void canSoftDeleteById() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
+
         // when
         underTest.save(company);
         underTest.deleteById(company.getId());
@@ -78,102 +81,92 @@ public class CompanyDaoTest {
         Assertions.assertThat(shouldBeSoftDeleted.getIsDeleted()).isTrue();
 
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void canGetById() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
         underTest.save(company);
+
         // when
         Company saved = underTest.get(company.getId());
+
         // then
         Assertions.assertThat(saved).isEqualTo(company);
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void getByIdShouldThrowEntityNotFoundExceptionIfSoftDeleted() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", true);
+                now().truncatedTo(SECONDS), "description", true);
         underTest.save(company);
+
         // when
         // then
         Assertions.assertThatThrownBy(() -> underTest.get(company.getId()))
                 .isInstanceOf(EntityNotFoundException.class)
-                .hasMessage(format("Company entity with id=%s is not present.", company.getId()));
+                .hasMessage(format(companyNotFoundMessage, company.getId()));
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void shouldExistByEmail() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
         underTest.save(company);
+
         // when
         boolean existsByEmail = underTest.existsByEmail(company.getEmail());
+
         // then
         Assertions.assertThat(existsByEmail).isTrue();
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void existsByEmailShouldBeFalseIfSoftDeleted() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", true);
+                now().truncatedTo(SECONDS), "description", true);
         underTest.save(company);
+
         // when
         boolean existsByEmail = underTest.existsByEmail(company.getEmail());
+
         // then
         Assertions.assertThat(existsByEmail).isFalse();
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
     public void existsByEmailShouldBeFalseIfEntityNotFound() {
         // given
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
         underTest.save(company);
+
         // when
         boolean existsByEmail = underTest.existsByEmail(company.getEmail() + "dsahjshfjdskfd");
+
         // then
         Assertions.assertThat(existsByEmail).isFalse();
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
@@ -185,14 +178,15 @@ public class CompanyDaoTest {
         Random random = new Random();
         for (int i = 0; i < pageSize + 1; i++) {
             company = new Company(null, "name", format("email%s@email.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                    now().truncatedTo(SECONDS), "description", false);
             softDeleted = new Company(null, "name", format("email%s@email.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", true);
+                    now().truncatedTo(SECONDS), "description", true);
             companies.add(company);
             companies.add(softDeleted);
             underTest.save(company);
             underTest.save(softDeleted);
         }
+
         // when
         List<Company> firstPage = underTest.getAll(0, Collections.emptyMap());
         List<Company> secondPage = underTest.getAll(1, Collections.emptyMap());
@@ -202,12 +196,9 @@ public class CompanyDaoTest {
         Assertions.assertThat(firstPage).noneMatch(company1 -> company1.getIsDeleted().equals(true));
         Assertions.assertThat(secondPage.size()).isEqualTo(companies.size() / 2 - pageSize);
         Assertions.assertThat(secondPage).noneMatch(company1 -> company1.getIsDeleted().equals(true));
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        companies.forEach(session::delete);
-        transaction.commit();
-        session.close();
+        deleteAllCompanies(companies);
     }
 
     @Test
@@ -219,9 +210,9 @@ public class CompanyDaoTest {
         Random random = new Random();
         for (int i = 0; i < pageSize / 2; i++) {
             company = new Company(null, "name", format("email%s@email.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                    now().truncatedTo(SECONDS), "description", false);
             company2 = new Company(null, "name", format("company%s@company.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                    now().truncatedTo(SECONDS), "description", false);
             companies.add(company);
             companies.add(company2);
             underTest.save(company);
@@ -237,12 +228,9 @@ public class CompanyDaoTest {
         // then
         Assertions.assertThat(firstPage.size()).isEqualTo(pageSize / 2);
         Assertions.assertThat(firstPage).allMatch(company1 -> company1.getEmail().contains(filterValue));
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        companies.forEach(session::delete);
-        transaction.commit();
-        session.close();
+        deleteAllCompanies(companies);
     }
 
     @Test
@@ -254,9 +242,9 @@ public class CompanyDaoTest {
         Random random = new Random();
         for (int i = 0; i < pageSize / 2; i++) {
             company = new Company(null, "name", format("email%s@email.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                    now().truncatedTo(SECONDS), "description", false);
             company2 = new Company(null, "absolutely different name", format("email%s@email.com", random.nextInt()),
-                    LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                    now().truncatedTo(SECONDS), "description", false);
             companies.add(company);
             companies.add(company2);
             underTest.save(company);
@@ -272,39 +260,33 @@ public class CompanyDaoTest {
         // then
         Assertions.assertThat(firstPage.size()).isEqualTo(pageSize / 2);
         Assertions.assertThat(firstPage).allMatch(company1 -> company1.getName().contains(filterValue));
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        companies.forEach(session::delete);
-        transaction.commit();
-        session.close();
+        deleteAllCompanies(companies);
     }
 
     @Test
     public void canUpdateCompany() {
         // given
-        Company updatedFields = new Company();
-        updatedFields.setDescription("upd description");
-        updatedFields.setName("upd name");
-        updatedFields.setEmail("updEmail@email.com");
+        Company updatedFields = new Company(null, "upd name",
+                "updEmail@email.com", null, "upd description", null);
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
         underTest.save(company);
 
         Company expected = new Company(company.getId(), updatedFields.getName(),
                 updatedFields.getEmail(), company.getCreated(), updatedFields.getDescription(),
                 company.getIsDeleted());
+
         // when
         underTest.update(company.getId(), updatedFields);
         Company updated = underTest.get(company.getId());
+
         // then
         Assertions.assertThat(updated).isEqualTo(expected);
+
         // clean up
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(company);
-        transaction.commit();
-        session.close();
+        deleteCompany(company);
     }
 
     @Test
@@ -312,15 +294,22 @@ public class CompanyDaoTest {
         // given
         Company updatedFields = new Company();
         Company company = new Company(null, "name", "email@email.com",
-                LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS), "description", false);
+                now().truncatedTo(SECONDS), "description", false);
         underTest.save(company);
 
         // when
         underTest.update(company.getId(), updatedFields);
         Company updated = underTest.get(company.getId());
+
         // then
         Assertions.assertThat(updated).isEqualTo(company);
+
         // clean up
+        deleteCompany(company);
+    }
+
+
+    private void deleteCompany(Company company) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         session.delete(company);
@@ -328,4 +317,11 @@ public class CompanyDaoTest {
         session.close();
     }
 
+    private void deleteAllCompanies(Collection<Company> companies) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        companies.forEach(session::delete);
+        transaction.commit();
+        session.close();
+    }
 }
