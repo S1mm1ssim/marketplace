2bf9fc2d2b7274af79b42be3a73f4c585df628db
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-11-30 13:32:07 +0300

    Move order-related exception messages to properties and upd tests

--- src/main/java/com/modsensoftware/marketplace/service/impl/OrderServiceImpl.java
+++ src/main/java/com/modsensoftware/marketplace/service/impl/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import com.modsensoftware.marketplace.exception.NoVersionProvidedException;
 import com.modsensoftware.marketplace.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -25,41 +26,39 @@ public class OrderServiceImpl implements OrderService {
 
     private final PositionDao positionDao;
 
+    @Value("${exception.message.noPositionVersionProvided}")
+    private String noPositionVersionProvidedMessage;
+    @Value("${exception.message.insufficientItemsInStock}")
+    private String insufficientItemsInStockMessage;
+    @Value("${exception.message.insufficientOrderAmount}")
+    private String insufficientOrderAmountMessage;
+
     @Override
     public void validateOrders(Collection<OrderDto> orders) {
-        if (log.isDebugEnabled()) {
-            log.debug("Validating orders: {}", orders);
-        }
+        log.debug("Validating orders: {}", orders);
         orders.forEach(orderDto -> {
-            if (log.isDebugEnabled()) {
-                log.debug("Validating order: {}", orderDto);
-            }
+            log.debug("Validating order: {}", orderDto);
             if (orderDto.getPositionVersion() == null) {
                 log.error("Provided order didn't contain position's version");
-                throw new NoVersionProvidedException(format("No version for position with id %s was provided",
+                throw new NoVersionProvidedException(format(noPositionVersionProvidedMessage,
                         orderDto.getPositionId()));
             }
             Position position = positionDao.get(orderDto.getPositionId());
             if (position.getAmount() < orderDto.getAmount().doubleValue()) {
                 log.error("Wanted amount is bigger than position with id {} has in stock", position.getId());
-                if (log.isDebugEnabled()) {
-                    log.debug("Wanted amount={}. Currently in stock={}",
-                            orderDto.getAmount(), position.getAmount());
-                }
+                log.debug("Wanted amount={}. Currently in stock={}",
+                        orderDto.getAmount(), position.getAmount());
                 throw new InsufficientItemsInStockException(
-                        format("Not enough items in stock for position with id=%s. Wanted amount=%s. "
-                                        + "Currently in stock=%s", position.getId(),
+                        format(insufficientItemsInStockMessage, position.getId(),
                                 orderDto.getAmount(), position.getAmount())
                 );
             }
             if (position.getMinAmount() > orderDto.getAmount().doubleValue()) {
                 log.error("Wanted amount is less than position's(id={}) minimum amount", position.getId());
-                if (log.isDebugEnabled()) {
-                    log.debug("Wanted amount: {}. Minimum value: {}", orderDto.getAmount(), position.getMinAmount());
-                }
+                log.debug("Wanted amount: {}. Minimum value: {}", orderDto.getAmount(), position.getMinAmount());
                 throw new InsufficientOrderAmountException(
-                        format("Wanted amount=%s is less than position's(id=%s) minimum amount=%s",
-                                orderDto.getAmount(), position.getId(), position.getMinAmount())
+                        format(insufficientOrderAmountMessage, orderDto.getAmount(),
+                                position.getId(), position.getMinAmount())
                 );
             }
             position.setAmount(position.getAmount() - orderDto.getAmount().doubleValue());
