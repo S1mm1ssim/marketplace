b48b3e818ff5c89c6df3dd230834eeaabef508ed
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-01 23:35:19 +0300

    Update Role enum to implement GrantedAuthority interface

--- src/main/java/com/modsensoftware/marketplace/enums/Role.java
+++ src/main/java/com/modsensoftware/marketplace/enums/Role.java
@@ -1,8 +1,21 @@
 package com.modsensoftware.marketplace.enums;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * @author andrey.demyanchik on 10/31/2022
  */
-public enum Role {
-    MANAGER, DIRECTOR, STORAGE_MANAGER
+@RequiredArgsConstructor
+public enum Role implements GrantedAuthority {
+    MANAGER("MANAGER"),
+    DIRECTOR("DIRECTOR"),
+    STORAGE_MANAGER("STORAGE_MANAGER");
+
+    private final String value;
+
+    @Override
+    public String getAuthority() {
+        return value;
+    }
 }
