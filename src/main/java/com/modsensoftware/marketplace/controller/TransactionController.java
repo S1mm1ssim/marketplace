54319a29a253f820cb2dcc766c0260c575aa1448
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-02 09:51:46 +0300

    Add preAuthorize on controllers

--- src/main/java/com/modsensoftware/marketplace/controller/TransactionController.java
+++ src/main/java/com/modsensoftware/marketplace/controller/TransactionController.java
@@ -6,6 +6,7 @@ import com.modsensoftware.marketplace.service.UserTransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,7 @@ public class TransactionController {
 
     private final UserTransactionService transactionService;
 
+    @PreAuthorize("hasAuthority('MANAGER')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/transactions")
     public void createUserTransaction(@Valid @RequestBody UserTransactionDto transactionDto) {
@@ -41,6 +43,7 @@ public class TransactionController {
         transactionService.createUserTransaction(transactionDto);
     }
 
+    @PreAuthorize("hasAuthority('MANAGER')")
     @GetMapping("/{userId}/transactions")
     public List<UserTransaction> getAllTransactionsForUser(
             @RequestParam(name = PAGE_FILTER_NAME, defaultValue = DEFAULT_PAGE_NUMBER) int pageNumber,
