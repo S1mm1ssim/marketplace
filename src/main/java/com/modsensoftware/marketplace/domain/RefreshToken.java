87a172a8d9d6cbc081f43e73457e9743b844eb57
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-03 16:23:54 +0300

    Implement redis service and entity that will be hashed

---
+++ src/main/java/com/modsensoftware/marketplace/domain/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.modsensoftware.marketplace.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+/**
+ * @author andrey.demyanchik on 12/2/2022
+ */
+@Getter
+@RedisHash("RefreshToken")
+public class RefreshToken {
+
+    @Id
+    private String id;
+    @Indexed
+    private String userEmail;
+    private String refreshToken;
+
+    public RefreshToken(String userEmail, String refreshToken) {
+        this.userEmail = userEmail;
+        this.refreshToken = refreshToken;
+    }
+}
