e0bb6661e4a82f1b35aa9e6b90fc4517b2889969
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-03 21:54:20 +0300

    Add lombok annotations on classes for tests

--- src/main/java/com/modsensoftware/marketplace/dto/request/JwtRequest.java
+++ src/main/java/com/modsensoftware/marketplace/dto/request/JwtRequest.java
@@ -1,6 +1,8 @@
 package com.modsensoftware.marketplace.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
@@ -10,6 +12,8 @@ import javax.validation.constraints.NotBlank;
  */
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequest {
 
     @NotBlank
