50f73b494053132f7ac91c076fd1f2d7ee26bef2
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-16 12:22:05 +0300

    Refactor Constants.java

--- src/main/java/com/modsensoftware/marketplace/constants/Constants.java
+++ src/main/java/com/modsensoftware/marketplace/constants/Constants.java
@@ -1,16 +1,18 @@
 package com.modsensoftware.marketplace.constants;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * @author andrey.demyanchik on 11/29/2022
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
 
+    public static final String PAGE_FILTER_NAME = "page";
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String EMAIL_FILTER_NAME = "email";
     public static final String NAME_FILTER_NAME = "name";
     public static final String CREATED_BETWEEN_FILTER_NAME = "created";
     public static final String COMPANY_ID_FILTER_NAME = "companyId";
-
-    private Constants() {
-    }
 }
