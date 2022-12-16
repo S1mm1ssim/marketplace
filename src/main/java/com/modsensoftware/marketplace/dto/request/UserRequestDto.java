7a82105572143b88e3635d0b9064586b1743e791
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-16 12:19:48 +0300

    Implement user request and response DTOs

--- src/main/java/com/modsensoftware/marketplace/dto/request/UserRequestDto.java
+++ src/main/java/com/modsensoftware/marketplace/dto/request/UserRequestDto.java
@@ -1,4 +1,4 @@
-package com.modsensoftware.marketplace.dto;
+package com.modsensoftware.marketplace.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,15 +8,14 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Pattern;
 
 /**
- * @author andrey.demyanchik on 11/3/2022
+ * @author andrey.demyanchik on 12/8/2022
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
+public class UserRequestDto {
     private String username;
-    @Pattern(regexp = "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*",
-            message = "Email must be valid.")
+    @Pattern(regexp = "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*", message = "Email must be valid.")
     private String email;
     private String name;
     @Length(min = 8, message = "Password must be at least 8 characters long")
