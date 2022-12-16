7b091499366120e732cf2a64c01bcecc43840fce
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-16 12:51:58 +0300

    Update integration test init scripts

--- src/test/resources/integration/user/userIntegrationTestData.sql
+++ src/test/resources/integration/user/userIntegrationTestData.sql
@@ -2,14 +2,3 @@ INSERT INTO company(id, name, email, created, description, is_deleted)
 VALUES (999, 'company', 'company@company.com', now(), 'description', false),
        (1000, 'company2', 'company2@company2.com', now(), 'description', false),
        (1001, 'softDeletedCompany', 'softDeleted@company.com', now(), 'description', true);
-
-
-INSERT INTO "user"(id, username, email, password, name, role, created, updated, company_id)
-VALUES ('b273ba0f-3b83-4cd4-a8bc-d44e5067ce6d', 'user', 'sqluser@user.com', 'password', 'full name',
-        'MANAGER', now(), now(), 1000),
-       ('097f575a-8e9d-4efe-ac01-d0d692262f4b', 'user', 'sqluser2@user.com', 'password', 'full name',
-        'MANAGER', now(), now(), 1000),
-       ('722cd920-e127-4cc2-93b9-e9b4a8f18873', 'toBeDeleted', 'toBeDeleted@mail.com', 'password', 'toBeDeleted',
-        'STORAGE_MANAGER', now(), now(), 999),
-       ('c048ef0e-fe46-4c65-9c01-d88af74ba0ab', 'softDeleted', 'softDeleted@user.com', 'password', 'full name',
-        'MANAGER', now(), now(), 1001);
--- src/test/resources/integration/user/userIntegrationTestTearDown.sql
+++ src/test/resources/integration/user/userIntegrationTestTearDown.sql
@@ -1,29 +1,3 @@
--- Created during integration test
-DELETE
-FROM "user"
-WHERE email = 'saveTest@test.com';
-
--- Users created during data script for integration test
-DELETE
-FROM "user"
-WHERE email = 'sqluser@user.com';
-
-DELETE
-FROM "user"
-WHERE email = 'sqluser2@user.com';
-
-DELETE
-FROM "user"
-WHERE email = 'softDeleted@user.com';
-
 DELETE
 FROM company
-WHERE id = 999;
-
-DELETE
-FROM company
-WHERE id = 1000;
-
-DELETE
-FROM company
-WHERE id = 1001;
+WHERE id IN (999, 1000, 1001);
