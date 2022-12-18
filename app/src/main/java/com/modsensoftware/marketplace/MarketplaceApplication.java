b4f034e2d531f0093bf30006cc5f0a6288826534
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-18 14:49:00 +0300

    Make application register in discovery service

--- app/src/main/java/com/modsensoftware/marketplace/MarketplaceApplication.java
+++ app/src/main/java/com/modsensoftware/marketplace/MarketplaceApplication.java
@@ -2,8 +2,10 @@ package com.modsensoftware.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
