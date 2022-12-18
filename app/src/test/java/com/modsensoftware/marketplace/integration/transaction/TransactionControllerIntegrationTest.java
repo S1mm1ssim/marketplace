6ebaf7120ddc2dc2c55053bebbab059465bcca4a
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-18 14:36:09 +0300

    Make multimodule maven project
    - Create parent pom
    - Create module app that represents previous project

--- app/src/test/java/com/modsensoftware/marketplace/integration/transaction/TransactionControllerIntegrationTest.java
+++ app/src/test/java/com/modsensoftware/marketplace/integration/transaction/TransactionControllerIntegrationTest.java
@@ -13,8 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.keycloak.admin.client.Keycloak;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.testcontainers.ext.ScriptUtils;
 
