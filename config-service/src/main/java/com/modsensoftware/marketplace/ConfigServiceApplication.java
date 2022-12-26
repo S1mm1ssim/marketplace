1a9656e1b4cd1965229adf6d56cea277a8ebc148
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-26 15:33:41 +0300

    Implement config-first approach

--- config-service/src/main/java/com/modsensoftware/marketplace/ConfigServiceApplication.java
+++ config-service/src/main/java/com/modsensoftware/marketplace/ConfigServiceApplication.java
@@ -3,13 +3,11 @@ package com.modsensoftware.marketplace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author andrey.demyanchik on 12/18/2022
  */
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServiceApplication {
 
