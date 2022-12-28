33c25af3089d33fa31b7bddef96a14a33adb7964
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2022-12-28 11:25:53 +0300

    Move config properties to separate class

--- src/main/java/com/modsensoftware/marketplace/config/KeycloakConfig.java
+++ src/main/java/com/modsensoftware/marketplace/config/KeycloakConfig.java
@@ -5,10 +5,6 @@ import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,32 +12,24 @@ import org.springframework.context.annotation.Configuration;
  * @author andrey.demyanchik on 12/8/2022
  */
 @Configuration
-@ConfigurationProperties(prefix = "idm")
-@ConfigurationPropertiesScan
-@ConstructorBinding
 @RequiredArgsConstructor
 public class KeycloakConfig {
 
-    private final String realmName;
-    private final String serverUrl;
-    private final String clientId;
-    private final String clientSecret;
-    private final String adminUsername;
-    private final String adminPassword;
-    private final int poolSize;
+    private final KeycloakConfigProperties properties;
 
     @Bean
     public Keycloak keycloak() {
+        System.out.println(properties);
         return KeycloakBuilder.builder()
-                .serverUrl(serverUrl)
-                .realm(realmName)
+                .serverUrl(properties.getServerUrl())
+                .realm(properties.getRealmName())
                 .grantType(OAuth2Constants.PASSWORD)
-                .username(adminUsername)
-                .password(adminPassword)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
+                .username(properties.getAdminUsername())
+                .password(properties.getAdminPassword())
+                .clientId(properties.getClientId())
+                .clientSecret(properties.getClientSecret())
                 .resteasyClient(new ResteasyClientBuilderImpl()
-                        .connectionPoolSize(poolSize)
+                        .connectionPoolSize(properties.getPoolSize())
                         .build()
                 )
                 .build();
