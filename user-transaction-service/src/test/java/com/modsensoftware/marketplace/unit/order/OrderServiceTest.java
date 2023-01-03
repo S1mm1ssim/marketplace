e6e0c312bae53523a2e495cfdb066de9cc2c047d
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-03 10:21:10 +0300

    Update service unit tests

--- user-transaction-service/src/test/java/com/modsensoftware/marketplace/unit/order/OrderServiceTest.java
+++ user-transaction-service/src/test/java/com/modsensoftware/marketplace/unit/order/OrderServiceTest.java
@@ -5,10 +5,8 @@ import com.modsensoftware.marketplace.dto.response.CompanyResponseDto;
 import com.modsensoftware.marketplace.dto.response.ItemResponseDto;
 import com.modsensoftware.marketplace.dto.response.PositionResponseDto;
 import com.modsensoftware.marketplace.dto.response.UserResponseDto;
-import com.modsensoftware.marketplace.dto.mapper.PositionMapper;
 import com.modsensoftware.marketplace.exception.InsufficientItemsInStockException;
 import com.modsensoftware.marketplace.exception.InsufficientOrderAmountException;
-import com.modsensoftware.marketplace.exception.NoVersionProvidedException;
 import com.modsensoftware.marketplace.service.OrderService;
 import com.modsensoftware.marketplace.service.impl.OrderServiceImpl;
 import com.modsensoftware.marketplace.service.impl.PositionClient;
@@ -36,12 +34,9 @@ public class OrderServiceTest {
 
     @Mock
     private PositionClient positionClient;
-    private final PositionMapper positionMapper = new PositionMapper();
 
     private OrderService underTest;
 
-    private static final String NO_POSITION_VERSION_PROVIDED_MESSAGE
-            = "No version for position with id %s was provided";
     private static final String INSUFFICIENT_ITEMS_IN_STOCK_MESSAGE
             = "Not enough items in stock for position with id=%s. Wanted amount=%s. Currently in stock=%s";
     private static final String INSUFFICIENT_ORDER_AMOUNT_MESSAGE
@@ -49,8 +44,7 @@ public class OrderServiceTest {
 
     @BeforeEach
     void setUp() {
-        underTest = new OrderServiceImpl(positionClient, positionMapper);
-        ReflectionTestUtils.setField(underTest, "noPositionVersionProvidedMessage", NO_POSITION_VERSION_PROVIDED_MESSAGE);
+        underTest = new OrderServiceImpl(positionClient);
         ReflectionTestUtils.setField(underTest, "insufficientItemsInStockMessage", INSUFFICIENT_ITEMS_IN_STOCK_MESSAGE);
         ReflectionTestUtils.setField(underTest, "insufficientOrderAmountMessage", INSUFFICIENT_ORDER_AMOUNT_MESSAGE);
     }
@@ -63,50 +57,31 @@ public class OrderServiceTest {
                 .item(ItemResponseDto.builder().id(UUID.randomUUID()).version(0L).build())
                 .company(CompanyResponseDto.builder().id(2L).build())
                 .createdBy(UserResponseDto.builder().id(UUID.randomUUID()).build())
-                .amount(30d).minAmount(1d).version(0L).build();
+                .amount(30d).minAmount(1d).build();
         PositionResponseDto pos2 = PositionResponseDto.builder()
                 .id(16L)
                 .item(ItemResponseDto.builder().id(UUID.randomUUID()).version(0L).build())
                 .company(CompanyResponseDto.builder().id(2L).build())
                 .createdBy(UserResponseDto.builder().id(UUID.randomUUID()).build())
-                .amount(30d).minAmount(1d).version(0L).build();
+                .amount(30d).minAmount(1d).build();
         List<OrderRequestDto> orders = new ArrayList<>();
-        orders.add(new OrderRequestDto(15L, new BigDecimal(5), 0L));
-        orders.add(new OrderRequestDto(16L, new BigDecimal(2), 0L));
+        orders.add(new OrderRequestDto(15L, new BigDecimal(5)));
+        orders.add(new OrderRequestDto(16L, new BigDecimal(2)));
         BDDMockito.when(positionClient.getPositionById(15L)).thenReturn(pos1);
         BDDMockito.when(positionClient.getPositionById(16L)).thenReturn(pos2);
 
         // when
         // then
         Assertions.assertThatNoException().isThrownBy(() -> underTest.validateOrders(orders));
-        BDDMockito.verify(positionClient).updatePosition(pos1.getId(), positionMapper.toPositionRequestDto(pos1));
-        BDDMockito.verify(positionClient).updatePosition(pos2.getId(), positionMapper.toPositionRequestDto(pos2));
-    }
-
-    @Test
-    public void shouldThrowEntityNotFoundExceptionIfNoPositionVersionProvided() {
-        // given
-        Long positionId = 16L;
-        List<OrderRequestDto> orders = new ArrayList<>();
-        // Null version provided
-        orders.add(new OrderRequestDto(positionId, new BigDecimal(4), null));
-
-        // when
-        // then
-        Assertions.assertThatThrownBy(() -> underTest.validateOrders(orders))
-                .isInstanceOf(NoVersionProvidedException.class)
-                .hasMessage(format(NO_POSITION_VERSION_PROVIDED_MESSAGE, positionId));
-        BDDMockito.verify(positionClient, BDDMockito.never()).getPositionById(BDDMockito.any());
-        BDDMockito.verify(positionClient, BDDMockito.never()).updatePosition(BDDMockito.any(), BDDMockito.any());
     }
 
     @Test
     public void shouldThrowInsufficientItemsInStockException() {
         // given
         PositionResponseDto pos = PositionResponseDto.builder()
-                .id(16L).amount(3d).minAmount(1d).version(0L).build();
+                .id(16L).amount(3d).minAmount(1d).build();
         List<OrderRequestDto> orders = new ArrayList<>();
-        orders.add(new OrderRequestDto(16L, new BigDecimal(4), 0L));
+        orders.add(new OrderRequestDto(16L, new BigDecimal(4)));
         BDDMockito.when(positionClient.getPositionById(16L)).thenReturn(pos);
 
         // when
@@ -115,16 +90,15 @@ public class OrderServiceTest {
                 .isInstanceOf(InsufficientItemsInStockException.class)
                 .hasMessage(format(INSUFFICIENT_ITEMS_IN_STOCK_MESSAGE, pos.getId(),
                         orders.get(0).getAmount(), pos.getAmount()));
-        BDDMockito.verify(positionClient, BDDMockito.never()).updatePosition(BDDMockito.any(), BDDMockito.any());
     }
 
     @Test
     public void shouldThrowInsufficientOrderAmountException() {
         // given
         PositionResponseDto pos = PositionResponseDto.builder()
-                .id(16L).amount(30d).minAmount(5d).version(0L).build();
+                .id(16L).amount(30d).minAmount(5d).build();
         List<OrderRequestDto> orders = new ArrayList<>();
-        orders.add(new OrderRequestDto(16L, new BigDecimal(4), 0L));
+        orders.add(new OrderRequestDto(16L, new BigDecimal(4)));
         BDDMockito.when(positionClient.getPositionById(16L)).thenReturn(pos);
 
         // when
@@ -133,6 +107,5 @@ public class OrderServiceTest {
                 .isInstanceOf(InsufficientOrderAmountException.class)
                 .hasMessage(format(INSUFFICIENT_ORDER_AMOUNT_MESSAGE, orders.get(0).getAmount(),
                         pos.getId(), pos.getMinAmount()));
-        BDDMockito.verify(positionClient, BDDMockito.never()).updatePosition(BDDMockito.any(), BDDMockito.any());
     }
 }
