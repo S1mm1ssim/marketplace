884e62242e462003978830883204c415490848fb
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-03 10:01:14 +0300

    Remove PositionClient put request

--- user-transaction-service/src/main/java/com/modsensoftware/marketplace/config/FeignErrorHandler.java
+++ user-transaction-service/src/main/java/com/modsensoftware/marketplace/config/FeignErrorHandler.java
@@ -5,8 +5,6 @@ import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.persistence.OptimisticLockException;
-
 
 /**
  * @author andrey.demyanchik on 12/21/2022
@@ -14,22 +12,16 @@ import javax.persistence.OptimisticLockException;
 @Slf4j
 public class FeignErrorHandler implements ErrorDecoder {
 
-    private static final int BAD_REQUEST_STATUS = 400;
     private static final int NOT_FOUND_STATUS = 404;
 
     private final ErrorDecoder errorDecoder = new Default();
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        switch (response.status()) {
-            case BAD_REQUEST_STATUS:
-                log.error("Position versions don't match");
-                return new OptimisticLockException("Bad Request: Position versions don't match");
-            case NOT_FOUND_STATUS:
-                log.error("Company entity not found");
-                return new EntityNotFoundException("Not Found: Company entity not found");
-            default:
-                return errorDecoder.decode(methodKey, response);
+        if (response.status() == NOT_FOUND_STATUS) {
+            log.error("Company entity not found");
+            return new EntityNotFoundException("Not Found: Company entity not found");
         }
+        return errorDecoder.decode(methodKey, response);
     }
 }
