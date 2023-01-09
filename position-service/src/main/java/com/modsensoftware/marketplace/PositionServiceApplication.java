d96b8d6a2ed18811f25ec21049c68ee2dda3dffa
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-09 23:39:24 +0300

    Configure reactive feign and clients

--- position-service/src/main/java/com/modsensoftware/marketplace/PositionServiceApplication.java
+++ position-service/src/main/java/com/modsensoftware/marketplace/PositionServiceApplication.java
@@ -1,17 +1,18 @@
 package com.modsensoftware.marketplace;
 
-import com.modsensoftware.marketplace.service.impl.CompanyClient;
 import com.modsensoftware.marketplace.service.impl.UserClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients(clients = {CompanyClient.class, UserClient.class})
+@EnableReactiveFeignClients(clients = {UserClient.class})
 @EnableCaching
+@EnableReactiveMongoAuditing
 public class PositionServiceApplication {
 
     public static void main(String[] args) {
--- position-service/src/main/java/com/modsensoftware/marketplace/service/impl/UserClient.java
+++ position-service/src/main/java/com/modsensoftware/marketplace/service/impl/UserClient.java
@@ -1,21 +1,20 @@
 package com.modsensoftware.marketplace.service.impl;
 
-import com.modsensoftware.marketplace.config.FeignErrorHandler;
+import com.modsensoftware.marketplace.config.ReactiveFeignConfig;
 import com.modsensoftware.marketplace.dto.response.UserResponseDto;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.UUID;
+import reactivefeign.spring.config.ReactiveFeignClient;
+import reactor.core.publisher.Mono;
 
 /**
  * @author andrey.demyanchik on 1/3/2023
  */
-@FeignClient(value = "USER-SERVICE", path = "/api/v1/users",
-        configuration = {FeignErrorHandler.class})
+@ReactiveFeignClient(value = "USER-SERVICE", path = "/api/v1/users",
+        configuration = {ReactiveFeignConfig.class})
 public interface UserClient {
 
     @RequestMapping(value = "{id}", produces = {"application/json"}, method = RequestMethod.GET)
-    UserResponseDto getUserById(@PathVariable(name = "id") UUID id);
+    Mono<UserResponseDto> getUserById(@PathVariable(name = "id") String id);
 }
