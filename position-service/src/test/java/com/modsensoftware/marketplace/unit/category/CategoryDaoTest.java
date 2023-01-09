e27764d2ff7f53de5f50df4df6fb2480bdff9b9e
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-09 23:46:07 +0300

    Reimplement DAO tests

--- position-service/src/test/java/com/modsensoftware/marketplace/unit/category/CategoryDaoTest.java
+++ position-service/src/test/java/com/modsensoftware/marketplace/unit/category/CategoryDaoTest.java
@@ -4,31 +4,37 @@ import com.modsensoftware.marketplace.dao.CategoryDao;
 import com.modsensoftware.marketplace.domain.Category;
 import com.modsensoftware.marketplace.exception.EntityNotFoundException;
 import com.modsensoftware.marketplace.unit.AbstractDaoTest;
-import org.assertj.core.api.Assertions;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.test.util.ReflectionTestUtils;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static java.lang.String.format;
 
 /**
  * @author andrey.demyanchik on 11/21/2022
  */
 public class CategoryDaoTest extends AbstractDaoTest {
 
-    @Autowired
     private CategoryDao underTest;
 
     @Value("${default.page.size}")
     private int pageSize;
     @Value("${exception.message.categoryNotFound}")
     private String categoryNotFoundMessage;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new CategoryDao(mongoTemplate);
+        ReflectionTestUtils.setField(underTest, "pageSize", pageSize);
+        ReflectionTestUtils.setField(underTest, "categoryNotFoundMessage", categoryNotFoundMessage);
+    }
 
     @Test
     public void canSaveCategory() {
@@ -37,33 +43,29 @@ public class CategoryDaoTest extends AbstractDaoTest {
         Category category = new Category(null, categoryName, "Root category", null);
 
         // when
-        underTest.save(category);
+        underTest.save(category).block();
 
         // then
-        Session session = sessionFactory.openSession();
-        Query<Category> query = session.createQuery("from Category where name = :root", Category.class);
-        query.setParameter("root", categoryName);
-        List<Category> resultList = query.getResultList();
-        Assertions.assertThat(resultList).hasSize(1);
-
+        underTest.get(category.getId())
+                .as(StepVerifier::create)
+                .expectNextMatches(category1 -> category1.equals(category))
+                .verifyComplete();
         // clean up
-        underTest.deleteById(resultList.get(0).getId());
+        underTest.deleteById(category.getId()).block();
     }
 
     @Test
     public void canDeleteCategory() {
         // given
         Category category = new Category(null, "root", "Root category", null);
-
+        underTest.save(category).block();
         // when
-        underTest.save(category);
-        underTest.deleteById(category.getId());
-
+        underTest.deleteById(category.getId()).block();
         // then
-        Session session = sessionFactory.openSession();
-        Query<Category> query = session.createQuery("from Category", Category.class);
-        List<Category> resultList = query.getResultList();
-        Assertions.assertThat(resultList).isEmpty();
+        mongoTemplate
+                .find(new Query(Criteria.where("name").is(category.getName())), Category.class)
+                .as(StepVerifier::create)
+                .verifyComplete();
     }
 
     @Test
@@ -71,20 +73,20 @@ public class CategoryDaoTest extends AbstractDaoTest {
         // given
         Category category1 = new Category(null, "root", "Root category", null);
         Category category2 = new Category(null, "category", "Category with parent", category1);
-        List<Category> expected = List.of(category1, category2);
-        underTest.save(category1);
-        underTest.save(category2);
+        underTest.save(category1).block();
+        underTest.save(category2).block();
 
         // when
-        List<Category> actual = underTest.getAll(0, Collections.emptyMap());
-
         // then
-        Assertions.assertThat(actual).hasSize(expected.size());
-        Assertions.assertThat(actual).isEqualTo(expected);
+        underTest.getAll(0, Collections.emptyMap())
+                .as(StepVerifier::create)
+                .expectNextCount(2)
+                .verifyComplete();
 
         // clean up
-        underTest.deleteById(actual.get(1).getId());
-        underTest.deleteById(actual.get(0).getId());
+        underTest.getAll(0, Collections.emptyMap())
+                .doOnNext(category -> underTest.deleteById(category.getId()))
+                .subscribe();
     }
 
     @Test
@@ -95,92 +97,89 @@ public class CategoryDaoTest extends AbstractDaoTest {
         for (int i = 0; i < pageSize + 1; i++) {
             testCategory = new Category(null, "category" + i, "", null);
             categories.add(testCategory);
-            underTest.save(testCategory);
+            underTest.save(testCategory).block();
         }
 
-        // when
-        List<Category> firstPageElemTotal = underTest.getAll(0, Collections.emptyMap());
-        List<Category> secondPageElemNumber = underTest.getAll(1, Collections.emptyMap());
+        Flux<Category> categoryFlux = Flux.fromIterable(categories);
 
-        // then
-        Assertions.assertThat(firstPageElemTotal).hasSize(pageSize);
-        Assertions.assertThat(secondPageElemNumber).hasSize(categories.size() - pageSize);
+        categoryFlux.subscribe(saved -> {
+            // when
+            // then
+            underTest.getAll(0, Collections.emptyMap())
+                    .as(StepVerifier::create)
+                    .expectNextCount(pageSize)
+                    .verifyComplete();
+            underTest.getAll(1, Collections.emptyMap())
+                    .as(StepVerifier::create)
+                    .expectNextCount(categories.size() - pageSize)
+                    .verifyComplete();
 
-        // clean up
-        categories.forEach(category -> underTest.deleteById(category.getId()));
+            // clean up
+            underTest.getAll(0, Collections.emptyMap())
+                    .doOnNext(category -> underTest.deleteById(category.getId()))
+                    .subscribe();
+            underTest.getAll(1, Collections.emptyMap())
+                    .doOnNext(category -> underTest.deleteById(category.getId()))
+                    .subscribe();
+        });
     }
 
     @Test
     public void canGetById() {
         // given
         Category category = new Category(null, "root", "Root category", null);
-        underTest.save(category);
-
+        underTest.save(category).block();
         // when
-        Category actual = underTest.get(category.getId());
-
-        // then
-        Assertions.assertThat(actual).isEqualTo(category);
-
+        //then
+        underTest.get(category.getId())
+                .as(StepVerifier::create)
+                .expectNext(category)
+                .verifyComplete();
         // clean up
-        underTest.deleteById(category.getId());
+        underTest.deleteById(category.getId()).block();
     }
+
 
     @Test
     public void shouldThrowEntityNotFoundExceptionWhenGetForNonexistentEntity() {
         // given
-        Long nonexistentId = 0L;
-
+        String nonexistentId = "0454545";
         // when
         // then
-        Assertions.assertThatThrownBy(() -> underTest.get(nonexistentId))
-                .isInstanceOf(EntityNotFoundException.class)
-                .hasMessage(format(categoryNotFoundMessage, nonexistentId));
+        underTest.get(nonexistentId)
+                .as(StepVerifier::create)
+                .expectErrorMatches(throwable -> throwable.getClass().equals(EntityNotFoundException.class)
+                        && throwable.getMessage().equals(String.format(categoryNotFoundMessage, nonexistentId)))
+                .verify();
     }
 
     @Test
     public void canUpdateCategory() {
         // given
-        Category parent1 = new Category(null, "parent1", "description", null);
-        Category parent2 = new Category(null, "parent2", "description", null);
+        Category parent1 = new Category(null, "parent1", "description1", null);
+        Category parent2 = new Category(null, "parent2", "description2", null);
         Category category = new Category(null, "category", "description", parent1);
-        Category updatedFields = new Category(null, "updatedCategory", "description", parent2);
-        underTest.save(parent1);
-        underTest.save(parent2);
-        underTest.save(category);
-        Category expected = new Category(category.getId(),
-                updatedFields.getName(), updatedFields.getDescription(), parent2);
+        underTest.save(parent1).block();
+        underTest.save(parent2).block();
+        underTest.save(category).block();
+
+        Category updatedFields = new Category(null, "updCategory", "updDescr",
+                Category.builder().id(parent2.getId()).build());
 
         // when
-        underTest.update(category.getId(), updatedFields);
-        Category actual = underTest.get(category.getId());
+        underTest.update(category.getId(), updatedFields).block();
 
         // then
-        Assertions.assertThat(actual).isEqualTo(expected);
+        Category expected = new Category(category.getId(), updatedFields.getName(),
+                updatedFields.getDescription(), parent2);
+        underTest.get(category.getId())
+                .as(StepVerifier::create)
+                .expectNext(expected)
+                .verifyComplete();
 
         // clean up
-        underTest.deleteById(category.getId());
-        underTest.deleteById(parent1.getId());
-        underTest.deleteById(parent2.getId());
-    }
-
-    @Test
-    public void noMutationOnNoUpdateFieldsProvided() {
-        // given
-        Category parent = new Category(null, "parent", "description", null);
-        Category category = new Category(null, "category", "description", parent);
-        underTest.save(parent);
-        underTest.save(category);
-
-        // when
-        underTest.update(category.getId(), new Category());
-        Category actual = underTest.get(category.getId());
-
-        // then
-        Assertions.assertThat(actual).isEqualTo(category);
-
-        // clean up
-        underTest.deleteById(category.getId());
-        underTest.deleteById(parent.getId());
+        underTest.deleteById(category.getId()).block();
+        underTest.deleteById(parent1.getId()).block();
+        underTest.deleteById(parent2.getId()).block();
     }
 }
