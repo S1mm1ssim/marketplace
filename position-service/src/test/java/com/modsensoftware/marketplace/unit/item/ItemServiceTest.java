2683d5f37c2814c4f6a28bd4efba0d0ea5522dc2
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-09 23:47:12 +0300

    Reimplement service tests

--- position-service/src/test/java/com/modsensoftware/marketplace/unit/item/ItemServiceTest.java
+++ position-service/src/test/java/com/modsensoftware/marketplace/unit/item/ItemServiceTest.java
@@ -1,11 +1,12 @@
 package com.modsensoftware.marketplace.unit.item;
 
+import com.modsensoftware.marketplace.dao.CategoryDao;
 import com.modsensoftware.marketplace.dao.ItemDao;
+import com.modsensoftware.marketplace.domain.Category;
 import com.modsensoftware.marketplace.domain.Item;
 import com.modsensoftware.marketplace.dto.ItemDto;
 import com.modsensoftware.marketplace.dto.mapper.ItemMapper;
 import com.modsensoftware.marketplace.service.impl.ItemServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +14,11 @@ import org.mapstruct.factory.Mappers;
 import org.mockito.BDDMockito;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.test.util.ReflectionTestUtils;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-import javax.persistence.OptimisticLockException;
 import java.util.UUID;
 
 /**
@@ -24,6 +27,8 @@ import java.util.UUID;
 @ExtendWith(MockitoExtension.class)
 public class ItemServiceTest {
 
+    @Mock
+    private CategoryDao categoryDao;
     @Mock
     private ItemDao itemDao;
 
@@ -36,41 +41,65 @@ public class ItemServiceTest {
 
     @BeforeEach
     void setUp() {
-        underTest = new ItemServiceImpl(itemDao, itemMapper);
+        underTest = new ItemServiceImpl(categoryDao, itemDao, itemMapper);
         ReflectionTestUtils.setField(underTest, "itemVersionsMismatchMessage", ITEM_VERSIONS_MISMATCH_MESSAGE);
     }
 
     @Test
     public void canUpdateItem() {
         // given
-        UUID id = UUID.randomUUID();
+        String id = UUID.randomUUID().toString();
         long version = 1L;
-        ItemDto updatedFields = new ItemDto(null, "description", null, version);
-        Item item = new Item(id, null, null, null, null, version);
-        BDDMockito.given(itemDao.get(id)).willReturn(item);
+        ItemDto updatedFields = ItemDto.builder().description("description").version(version).build();
+        Item item = Item.builder().id(id).version(version).build();
+        Item expected = Item.builder().id(id).description("description").version(version + 1).build();
+        BDDMockito.given(itemDao.get(id)).willReturn(Mono.just(item));
+        BDDMockito.given(itemDao.update(id, itemMapper.toItem(updatedFields))).willReturn(Mono.just(expected));
 
         // when
-        underTest.updateItem(id, updatedFields);
+        underTest.updateItem(id, updatedFields)
+                // then
+                .as(StepVerifier::create)
+                .expectNext(expected)
+                .verifyComplete();
+    }
 
-        // then
-        BDDMockito.verify(itemDao).update(id, itemMapper.toItem(updatedFields));
+    @Test
+    public void canCreateItem() {
+        // given
+        String categoryId = "12345";
+        Category category = Category.builder().id(categoryId).name("category").build();
+        ItemDto toBeSaved = ItemDto.builder().name("name").description("descr").version(0L).categoryId(categoryId).build();
+        Item expected = Item.builder().name("name").description("descr")
+                .category(category).version(0L).build();
+        BDDMockito.given(categoryDao.get(categoryId)).willReturn(Mono.just(category));
+        BDDMockito.given(itemDao.save(BDDMockito.any())).willReturn(Mono.just(expected));
+
+        // when
+        underTest.createItem(toBeSaved)
+                // then
+                .as(StepVerifier::create)
+                .expectNext(expected)
+                .verifyComplete();
     }
 
     @Test
     public void shouldThrowExceptionWhenItemVersionsMismatch() {
         // given
-        UUID id = UUID.randomUUID();
+        String id = UUID.randomUUID().toString();
         long version = 1L;
         long differentVersion = 2L;
-        ItemDto updatedFields = new ItemDto(null, "description", null, version);
-        Item item = new Item(id, null, null, null, null, differentVersion);
-        BDDMockito.given(itemDao.get(id)).willReturn(item);
+        ItemDto updatedFields = ItemDto.builder().description("description").version(version).build();
+        Item item = Item.builder().id(id).version(differentVersion).build();
+        BDDMockito.given(itemDao.get(id)).willReturn(Mono.just(item));
 
         // when
+        Mono<Item> itemMono = underTest.updateItem(id, updatedFields);
+
         // then
-        Assertions.assertThatThrownBy(() -> underTest.updateItem(id, updatedFields))
-                .isInstanceOf(OptimisticLockException.class)
-                .hasMessage(ITEM_VERSIONS_MISMATCH_MESSAGE);
-        BDDMockito.verify(itemDao, BDDMockito.never()).update(BDDMockito.any(), BDDMockito.any());
+        itemMono.as(StepVerifier::create)
+                .expectErrorMatches(throwable -> throwable.getClass().equals(OptimisticLockingFailureException.class)
+                        && throwable.getMessage().equals(ITEM_VERSIONS_MISMATCH_MESSAGE))
+                .verify();
     }
 }
