173237ace3c91af41a026a8c51aa1ad1299c7403
S1mm1ssim <Andrdemyan4ik@yandex.ru>
2023-01-09 23:40:38 +0300

    Refactor entity classes: migrate to mongo

--- position-service/src/main/java/com/modsensoftware/marketplace/domain/Category.java
+++ position-service/src/main/java/com/modsensoftware/marketplace/domain/Category.java
@@ -1,45 +1,25 @@
 package com.modsensoftware.marketplace.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
 
 /**
  * @author andrey.demyanchik on 10/31/2022
  */
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "category")
+@Document(collection = "categories")
 public class Category {
 
-    public static final String ID_FIELD_NAME = "id";
-    public static final String NAME_FIELD_NAME = "name";
-    public static final String DESCRIPTION_FIELD_NAME = "description";
-    public static final String PARENT_CATEGORY_FIELD_NAME = "parent";
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name", nullable = false)
+    @MongoId
+    private String id;
     private String name;
-
-    @Column(name = "description")
     private String description;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "parent_category")
     private Category parent;
 }
